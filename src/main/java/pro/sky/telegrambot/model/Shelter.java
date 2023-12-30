573af31576e51bb1884623f15c381f720f7227a6
Alexlikhanov2011 <Likhanov-2011@yandex.ru>
2023-12-31 01:06:47 +0300

    сделал модель,сервис,контроллер,и тесты на сервис

--- src/main/java/pro/sky/telegrambot/model/Shelter.java
+++ src/main/java/pro/sky/telegrambot/model/Shelter.java
@@ -1,16 +1,35 @@
 package pro.sky.telegrambot.model;
 
+import javax.persistence.*;
 import java.util.Objects;
 
+@Entity
+@Table(name = "shelter")
 public class Shelter {
-    private Long id;
+    @Id
+    @Enumerated(EnumType.STRING)
+    private ShelterId id;
     private String name;
+    private String information;
+    private String timetable;
+    private String address;
+    private String security;
+    private String safetyPrecautions;
+    private String rules;
+    private String documents;
+    private String transportation;
+    private String childAccomodation;
+    private String adultAccomodation;
+    private String invalidAccomodation;
+    private String communication;
+    private String cynologists;
+    private String refusalReasons;
 
-    public Long getId() {
+    public ShelterId getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(ShelterId id) {
         this.id = id;
     }
 
@@ -22,24 +41,128 @@ public class Shelter {
         this.name = name;
     }
 
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    public String getTimetable() {
+        return timetable;
+    }
+
+    public void setTimetable(String timetable) {
+        this.timetable = timetable;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSecurity() {
+        return security;
+    }
+
+    public void setSecurity(String security) {
+        this.security = security;
+    }
+
+    public String getSafetyPrecautions() {
+        return safetyPrecautions;
+    }
+
+    public void setSafetyPrecautions(String safetyPrecautions) {
+        this.safetyPrecautions = safetyPrecautions;
+    }
+
+    public String getRules() {
+        return rules;
+    }
+
+    public void setRules(String rules) {
+        this.rules = rules;
+    }
+
+    public String getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(String documents) {
+        this.documents = documents;
+    }
+
+    public String getTransportation() {
+        return transportation;
+    }
+
+    public void setTransportation(String transportation) {
+        this.transportation = transportation;
+    }
+
+    public String getChildAccomodation() {
+        return childAccomodation;
+    }
+
+    public void setChildAccomodation(String childAccomodation) {
+        this.childAccomodation = childAccomodation;
+    }
+
+    public String getAdultAccomodation() {
+        return adultAccomodation;
+    }
+
+    public void setAdultAccomodation(String adultAccomodation) {
+        this.adultAccomodation = adultAccomodation;
+    }
+
+    public String getInvalidAccomodation() {
+        return invalidAccomodation;
+    }
+
+    public void setInvalidAccomodation(String invalidAccomodation) {
+        this.invalidAccomodation = invalidAccomodation;
+    }
+
+    public String getCommunication() {
+        return communication;
+    }
+
+    public void setCommunication(String communication) {
+        this.communication = communication;
+    }
+
+    public String getCynologists() {
+        return cynologists;
+    }
+
+    public void setCynologists(String cynologists) {
+        this.cynologists = cynologists;
+    }
+
+    public String getRefusalReasons() {
+        return refusalReasons;
+    }
+
+    public void setRefusalReasons(String refusalReasons) {
+        this.refusalReasons = refusalReasons;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Shelter shelter = (Shelter) o;
-        return Objects.equals(id, shelter.id) && Objects.equals(name, shelter.name);
+        return Objects.equals(id, shelter.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    @Override
-    public String toString() {
-        return "Shelter{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        return Objects.hash(id);
     }
 }
