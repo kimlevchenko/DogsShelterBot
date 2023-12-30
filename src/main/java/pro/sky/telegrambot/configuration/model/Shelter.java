4533a5b326bd09de03cfb3a4f51c38bbfe52f1e8
Alexlikhanov2011 <Likhanov-2011@yandex.ru>
2023-12-30 13:13:51 +0300

    начало создания класса приюта

---
+++ src/main/java/pro/sky/telegrambot/configuration/model/Shelter.java
@@ -0,0 +1,45 @@
+package pro.sky.telegrambot.configuration.model;
+
+import java.util.Objects;
+
+public class Shelter {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Shelter shelter = (Shelter) o;
+        return Objects.equals(id, shelter.id) && Objects.equals(name, shelter.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Shelter{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
