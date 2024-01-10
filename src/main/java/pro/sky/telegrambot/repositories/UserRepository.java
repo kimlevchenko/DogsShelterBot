b6075068d4da6aab484bb8631784d64cfd8cac90
elenaermolaeva <elena161284@mail.ru>
2024-01-10 20:34:51 +0300

    черновик2

--- src/main/java/pro/sky/telegrambot/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package pro.sky.telegrambot.repositories;
-
-
-import com.pengrad.telegrambot.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
