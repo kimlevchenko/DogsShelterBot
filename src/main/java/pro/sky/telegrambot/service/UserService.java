4fbb2456131ec62e70b6d3c172f58129a83aa1ec
elenaermolaeva <elena161284@mail.ru>
2024-01-12 16:00:10 +0300

    коммит для перехода на свою ветку1

--- src/main/java/pro/sky/telegrambot/service/UserService.java
+++ src/main/java/pro/sky/telegrambot/service/UserService.java
@@ -1,19 +1,15 @@
 package pro.sky.telegrambot.service;
 
-
 import com.pengrad.telegrambot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-
 import pro.sky.telegrambot.repositories.UserRepository;
 
 import java.util.List;
 
-;
-
 @Service
 public class UserService {
-    private final UserRepository UserRepository;
+    private final pro.sky.telegrambot.repositories.UserRepository UserRepository;
 
     public UserService(UserRepository UserRepository) {
         this.UserRepository = UserRepository;
