8d0b56a39359ac8bd05858d1787c643716ac6f10
elenaermolaeva <elena161284@mail.ru>
2024-01-17 12:41:55 +0300

    переписала: animal (service, controller), Repository(Animal,Cat,Dog), Report(Cat^ Dog), Adoption(Cat, Dog) Запустилось!

---
+++ src/main/java/pro/sky/telegrambot/adoption/Adoption.java
@@ -0,0 +1,96 @@
+package pro.sky.telegrambot.adoption;
+
+import pro.sky.telegrambot.model.entity.Animal;
+import pro.sky.telegrambot.model.entity.Cat;
+import pro.sky.telegrambot.model.entity.Dog;
+import pro.sky.telegrambot.model.entity.User;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@MappedSuperclass //указывает, что этот класс НЕ является сущностью JPA,
+// но его поля будут унаследованы подклассами, которые будут сущностями JPA.
+
+public class Adoption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    private User user;
+    //Дата усыновления. За один день один пользователь может усыновить только одно животное.
+    private LocalDate date;
+    //Дата окончания испытательного срока. Назначается волонтером.
+    //В интервале от date до trialDate бот ждет отчеты
+    //При отборе животного волонтер устанавливает trialDate в 01.01.2001
+    private LocalDate trialDate;
+
+    public Adoption() {
+    }
+
+    public Adoption(User user, LocalDate date, LocalDate trialDate) {
+        this.user = user;
+        this.date = date;
+        this.trialDate = trialDate;
+    }
+
+    public Animal getAnimal() {
+        return null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    } //для тестов
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalDate getTrialDate() {
+        return trialDate;
+    }
+
+    public void setTrialDate(LocalDate trialDate) {
+        this.trialDate = trialDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Adoption that = (Adoption) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Adoption{" +
+                "id=" + id +
+                ", user=" + user +
+                ", date=" + date +
+                ", trialDate=" + trialDate +
+                '}';
+    }
+}
---
+++ src/main/java/pro/sky/telegrambot/adoption/CatAdoption.java
@@ -0,0 +1,44 @@
+package pro.sky.telegrambot.adoption;
+
+
+import pro.sky.telegrambot.model.entity.Cat;
+import pro.sky.telegrambot.model.entity.Dog;
+import pro.sky.telegrambot.model.entity.User;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "cat_adoption") //в этой таблице базы данных будут храниться объекты этого класса
+
+public class CatAdoption extends Adoption {
+    //Питомец. не animal_id, а целый Animal, чтобы возвратить в коллекции усыновлений и описание питомца тоже
+    @ManyToOne
+    private Cat animal;
+
+    @Override
+    public Cat getAnimal() {
+        return animal;
+    }
+
+    public CatAdoption(User user, Cat animal, LocalDate date, LocalDate trialDate) {
+        super(user, date, trialDate);
+        this.animal = animal;
+    }
+
+    public CatAdoption() {
+    }
+
+    @Override
+    public String toString() {
+        return "CatAdoption{" +
+                "id=" + getId() +
+                ", user=" + getUser() +
+                ", animal=" + animal +
+                ", date=" + getDate() +
+                ", trialDate=" + getTrialDate() +
+                "}";
+    }
+}
