dd850bf214e4b55c87a039e132b6f6aaaa930997
kimlevcenko <levchenko.kim@icloud.com>
2024-01-29 09:40:36 +0500

    fix Animal.java

--- src/main/java/pro/sky/telegrambot/model/animal/Animal.java
+++ src/main/java/pro/sky/telegrambot/model/animal/Animal.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 import java.util.Arrays;
 import java.util.Objects;
 
-@MappedSuperclass
+@Entity
 public class Animal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
